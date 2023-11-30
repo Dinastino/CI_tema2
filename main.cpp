46de2dc15d96e3f35e80d4c7bf673fb25a02b719
Carlos <corammun@myuax.com>
2023-11-30 11:50:50 +0100

    3

--- main.cpp
+++ main.cpp
@@ -2,3 +2,7 @@
 #include <string>
 #include <cstdio>
 using namespace  std;
+struct ColorConsole{
+    static constexpr auto fg_blue = "\033[34m";
+    static constexpr auto fg_white = "\033[47m";
+};
